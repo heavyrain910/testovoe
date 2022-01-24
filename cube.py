b4b29defaed5de7ac38e0b99fdf35765bb6783ab
heavyrain910 <98119620+heavyrain910@users.noreply.github.com>
2022-01-24 14:38:39 +0400

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,5 +1,11 @@
-from itertools import *
+from itertools import *  #все_перестановки
 count = 0
-for i in permutations('12345678'):
+for i in permutations([1,2,3,4,5,6,7,8], 8):
+  count+=1  
+print(count)
+
+#перестановки_без_повторений
+count = 0
+for i in permutations([1,2,3,4,5,6,7,8], 1):
   count+=1  
 print(count)
